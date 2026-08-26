bf0f61e3875a12888c1d56429af61e7437797ebf
rnasir531 <nasirsajjad531@gmail.com>
2026-08-26 14:20:06 +0500

    pushed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const Login = () => {
+const Login = ({ mode = "login" }) => {
   return (
-    <div>Login</div>
+    <div className="min-h-screen w-full bg-[url('/login_bg.png')] text-slate-800 p-4 md:p-6
+    lg:p-8 flex items-center justify-center font-sans">
+      <div className='w-full flex justify-center py-2'>
+        <p>{mode}</p>
+      </div>
+    </div>
   )
 }
 
